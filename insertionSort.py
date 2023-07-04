a513cd060397ca7b35b78abd7200d8c896e4ad46
SatyaJay07 <90130760+SatyaJay07@users.noreply.github.com>
2023-07-04 22:55:20 +0530

    Add files via upload

---
+++ insertionSort.py
@@ -0,0 +1,12 @@
+def insertionSort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j=j-1
+        arr[j+1]=key
+    print(arr)
+ar=[5,4,3,2]
+insertionSort(ar)
+print("end")
